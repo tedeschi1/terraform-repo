3fb36959a1110ee47e3cae6edfbe2083e3bf287a
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2023-11-12 14:25:31 -0600

    vars updates

--- vnet169/main.tf
+++ vnet169/main.tf
@@ -5,14 +5,14 @@ resource "azurerm_resource_group" "rg-vnet169" {
 
 resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg-vnet169.location
+  resource_group_name = azurerm_resource_group.rg-vnet169.name
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = var.vnet-name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg-vnet169.location
+  resource_group_name = azurerm_resource_group.rg-vnet169.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
